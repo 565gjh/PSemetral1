62722a821a69e2336f8c298f615d9d6dc03e00a5
Cristobal Vasquez <cri.vasquezn@duocuc.cl>
2026-06-12 17:27:46 -0400

    Delete MSAutores directory

--- MSAutores/src/main/java/com/example/MSAutores/repository/autorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.MSAutores.repository;
-
-
-import com.example.MSAutores.model.Autor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface autorRepository extends JpaRepository<Autor, Long> {
-}
